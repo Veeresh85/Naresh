a6e602dedd16a7933ce7fc508177b191623bf3e7
Veeresh85 <veeresh.settyb@gmail.com>
2025-02-21 19:13:58 +0100

    Add files via upload

---
+++ src/main/java/com/example/Naresh/NareshApplication.java
@@ -0,0 +1,13 @@
+package com.example.Naresh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NareshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NareshApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Naresh/NareshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Naresh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NareshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
